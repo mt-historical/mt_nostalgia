0ffccce492d2db73c0f3f0b70326a489da0bcdc9
DanDuncombe <fishermanduncombe@gmail.com>
2013-10-20 00:16:47 +0100

    New Stuff

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -214,55 +214,6 @@ minetest.register_node("default:cloud", {
 	groups = {not_in_creative_inventory=1},
 })
 
-minetest.register_node("default:water_flowing", {
-	description = "Flowing Water",
-	inventory_image = "water.png",
-	drawtype = "flowingliquid",
-	special_tiles = {
-		{
-			image="water_flowing.png",
-			backface_culling=true,
-			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=0.8}
-		},
-	},
-	alpha = WATER_ALPHA,
-	paramtype = "light",
-	paramtype2 = "flowingliquid",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	drop = "",
-	drowning = 1,
-	liquidtype = "flowing",
-	liquid_alternative_flowing = "default:water_flowing",
-	liquid_alternative_source = "default:water_source",
-	liquid_viscosity = WATER_VISC,
-	post_effect_color = {a=64, r=100, g=100, b=200},
-	groups = {water=3, liquid=3, puts_out_fire=1, not_in_creative_inventory=1, freezes=1, melt_around=1},
-})
-
-minetest.register_node("default:water_source", {
-	description = "Water Source",
-	inventory_image = "water.png",
-	drawtype = "liquid",
-	tiles = {"water.png"},
-	alpha = WATER_ALPHA,
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	drop = "",
-	drowning = 1,
-	liquidtype = "source",
-	liquid_alternative_flowing = "default:water_flowing",
-	liquid_alternative_source = "default:water_source",
-	liquid_viscosity = WATER_VISC,
-	post_effect_color = {a=64, r=100, g=100, b=200},
-	groups = {water=3, liquid=3, puts_out_fire=1, freezes=1},
-})
-
 minetest.register_node("default:torch", {
 	description = "Torch",
 	drawtype = "torchlike",
@@ -674,4 +625,106 @@ minetest.register_abm({
 		
 		inv:set_stack("fuel", 1, afterfuel.items[1])
 	end,
+})
+
+minetest.register_node("default:water_flowing", {
+	description = "Flowing Water",
+	inventory_image = "water.png",
+	drawtype = "flowingliquid",
+	special_tiles = {
+		{
+			image="water_flowing.png",
+			backface_culling=true,
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=0.8}
+		},
+	},
+	alpha = WATER_ALPHA,
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	drop = "",
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "default:water_flowing",
+	liquid_alternative_source = "default:water_source",
+	liquid_viscosity = WATER_VISC,
+	post_effect_color = {a=64, r=100, g=100, b=200},
+	groups = {water=3, liquid=3, puts_out_fire=1, not_in_creative_inventory=1, freezes=1, melt_around=1},
+})
+
+minetest.register_node("default:water_source", {
+	description = "Water Source",
+	inventory_image = "water.png",
+	drawtype = "liquid",
+	tiles = {"water.png"},
+	alpha = WATER_ALPHA,
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	drop = "",
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "default:water_flowing",
+	liquid_alternative_source = "default:water_source",
+	liquid_viscosity = WATER_VISC,
+	post_effect_color = {a=64, r=100, g=100, b=200},
+	groups = {water=3, liquid=3, puts_out_fire=1, freezes=1},
+})
+
+minetest.register_node("default:lava_flowing", {
+	description = "Flowing Lava",
+	inventory_image = "lava.png",
+	drawtype = "flowingliquid",
+	special_tiles = {
+		{
+			image="lava_flowing.png",
+			backface_culling=false,
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2}
+		},
+	},
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	light_source = 14,
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	drop = "",
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "default:lava_flowing",
+	liquid_alternative_source = "default:lava_source",
+	liquid_viscosity = LAVA_VISC,
+	liquid_renewable = false,
+	damage_per_second = 4*2,
+	post_effect_color = {a=192, r=255, g=64, b=0},
+	groups = {lava=3, liquid=2, hot=3, igniter=1, not_in_creative_inventory=1},
+})
+
+minetest.register_node("default:lava_source", {
+	description = "Lava Source",
+	inventory_image = "lava.png",
+	drawtype = "liquid",
+	tiles = {"lava.png"},
+	paramtype = "light",
+	light_source = 14,
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	drop = "",
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "default:lava_flowing",
+	liquid_alternative_source = "default:lava_source",
+	liquid_viscosity = LAVA_VISC,
+	liquid_renewable = false,
+	damage_per_second = 4*2,
+	post_effect_color = {a=192, r=255, g=64, b=0},
+	groups = {lava=3, liquid=2, hot=3, igniter=1},
 })
