697643c8670279c94282016c306912033b7c9ead
DanDuncombe <fishermanduncombe@gmail.com>
2013-10-20 18:05:43 +0100

    New Stuff

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -134,7 +134,7 @@ minetest.register_node("default:sand", {
 	tiles = {"sand.png"},
 	inventory_image = "sand.png",
 	is_ground_content = true,
-	groups = {crumbly=3, falling_node=1, sand=1},
+	groups = {crumbly=3, sand=1},
 	sounds = default.node_sound_sand_defaults(),
 })
 
